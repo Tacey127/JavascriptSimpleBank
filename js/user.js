fec6302db14cb312350f9b6a0ea936e74ca4ba10
Tacey127 <jtacey@hotmail.co.uk>
2021-02-21 00:20:47 +0000

    Cookie Introduction
    Adding persistence to swap between html page layouts

--- js/user.js
+++ js/user.js
@@ -1,5 +1,45 @@
 {
 
+    function setCookie(cname,cvalue,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+    function checkCookie() {
+        var user=getCookie("jsBank");
+        if (user != "") {
+
+          alert("js Bank Cookie Exists");
+
+        } else {
+            /*
+           user = prompt("Please enter your name:","");
+           if (user != "" && user != null) {
+             setCookie("username", user, 30);
+           }
+           */
+        }
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+
+
     class Bank
     {
         static #users = [];
@@ -25,7 +65,6 @@
             return false;
         }
     }
-
 
     class User{
 
@@ -53,7 +92,7 @@
     {
         AccessCustomerAccount(id)
         {
-        
+            
         }
     }
 
